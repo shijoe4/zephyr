shijoe4/zephyr

commit 2397ec753ca6fb4f2a3fadffe8ba09f8015f58b3

drivers: flash: nrf_qspi_nor: Fix no multithreading configuration
File was failing to compile when multithreading was off.

Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>

drivers/flash/nrf_qspi_nor.c
@@ -399,15 +399,19 @@ static void anomaly_122_uninit(const struct device *dev)
 
 	qspi_lock(dev);
 
-#ifdef CONFIG_MULTITHREADING
-	/* The last thread to finish using the driver uninit the QSPI */
-	(void) k_sem_take(&dev_data->count, K_NO_WAIT);
-	last = (k_sem_count_get(&dev_data->count) == 0);
-#endif
+	if (IS_ENABLED(CONFIG_MULTITHREADING)) {
+		/* The last thread to finish using the driver uninit the QSPI */
+		(void) k_sem_take(&dev_data->count, K_NO_WAIT);
+		last = (k_sem_count_get(&dev_data->count) == 0);
+	}
 
 	if (last) {
 		while (nrfx_qspi_mem_busy_check() != NRFX_SUCCESS) {
-			k_msleep(50);
+			if (IS_ENABLED(CONFIG_MULTITHREADING)) {
+				k_msleep(50);
+			} else {
+				k_busy_wait(50000);
+			}
 		}
 
 		nrf_gpio_cfg_output(QSPI_PROP_AT(csn_pins, 0));

commit c74e04471b8694e5a3472c54a571af514c6fe371

drivers: flash: nrf_qspi_nor: Fix no multithreading compilation
Fixing error introduced in 2397ec753ca6fb4f2a3fadffe8ba09f8015f58b3
where ifdef was converted to IS_ENABLED. Ifdef was required because
element in the struct does not exist when multithreading is disabled.

Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>

drivers/flash/nrf_qspi_nor.c
@@ -390,7 +390,6 @@ static int anomaly_122_init(const struct device *dev)
 
 static void anomaly_122_uninit(const struct device *dev)
 {
-	struct qspi_nor_data *dev_data = get_dev_data(dev);
 	bool last = true;
 
 	if (!nrf52_errata_122()) {
@@ -399,11 +398,13 @@ static void anomaly_122_uninit(const struct device *dev)
 
 	qspi_lock(dev);
 
-	if (IS_ENABLED(CONFIG_MULTITHREADING)) {
-		/* The last thread to finish using the driver uninit the QSPI */
-		(void) k_sem_take(&dev_data->count, K_NO_WAIT);
-		last = (k_sem_count_get(&dev_data->count) == 0);
-	}
+#ifdef CONFIG_MULTITHREADING
+	struct qspi_nor_data *dev_data = get_dev_data(dev);
+
+	/* The last thread to finish using the driver uninit the QSPI */
+	(void) k_sem_take(&dev_data->count, K_NO_WAIT);
+	last = (k_sem_count_get(&dev_data->count) == 0);
+#endif
 
 	if (last) {
 		while (nrfx_qspi_mem_busy_check() != NRFX_SUCCESS) {