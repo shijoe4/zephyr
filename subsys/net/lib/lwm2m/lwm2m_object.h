jtguggedal/sdk-zephyr

net: lwm2m: Added SenML JSON support for Read & Write
LwM2M SenML JSON contain format support for Read / Write operation.
Added Kconfig configurable for enable SenML JSON format.
LwM2m read validate read operation and report out of memory.

Signed-off-by: Juha Heiskanen <juha.heiskanen@nordicsemi.no>

subsys/net/lib/lwm2m/lwm2m_object.h
@@ -137,6 +137,8 @@ BUILD_ASSERT(CONFIG_LWM2M_COAP_BLOCK_SIZE <= CONFIG_LWM2M_COAP_MAX_MSG_SIZE,
 /* buffer util macros */
 #define CPKT_BUF_WRITE(cpkt)	(cpkt)->data, &(cpkt)->offset, (cpkt)->max_len
 #define CPKT_BUF_READ(cpkt)	(cpkt)->data, (cpkt)->max_len
+#define CPKT_BUF_PTR(cpkt)	((cpkt)->data + (cpkt)->offset)
+#define CPKT_BUF_SIZE(cpkt)	((cpkt)->max_len - (cpkt)->offset)
 
 struct lwm2m_engine_obj;
 struct lwm2m_message;
@@ -398,9 +400,22 @@ struct lwm2m_opaque_context {
 	size_t remaining;
 };
 
+struct lwm2m_senml_json_context {
+	bool base_name_stored : 1;
+	bool full_name_true : 1;
+	uint8_t base64_buf_len : 2;
+	uint8_t base64_mod_buf[3];
+	uint8_t json_flags;
+	struct lwm2m_obj_path base_name_path;
+	uint8_t resource_path_level;
+};
+
 struct lwm2m_block_context {
 	struct coap_block_context ctx;
 	struct lwm2m_opaque_context opaque;
+#if defined(CONFIG_LWM2M_RW_SENML_JSON_SUPPORT)
+struct lwm2m_senml_json_context senml_json_ctx;
+#endif
 	int64_t timestamp;
 	uint32_t expected;
 	uint8_t token[8];

shijoe4/zephyr

subsys/net/lib/lwm2m/lwm2m_object.h
@@ -137,6 +137,8 @@ BUILD_ASSERT(CONFIG_LWM2M_COAP_BLOCK_SIZE <= CONFIG_LWM2M_COAP_MAX_MSG_SIZE,
 /* buffer util macros */
 #define CPKT_BUF_WRITE(cpkt)	(cpkt)->data, &(cpkt)->offset, (cpkt)->max_len
 #define CPKT_BUF_READ(cpkt)	(cpkt)->data, (cpkt)->max_len
+#define CPKT_BUF_W_PTR(cpkt)	((cpkt)->data + (cpkt)->offset)
+#define CPKT_BUF_W_SIZE(cpkt)	((cpkt)->max_len - (cpkt)->offset)
 
 struct lwm2m_engine_obj;
 struct lwm2m_message;
@@ -398,9 +400,22 @@ struct lwm2m_opaque_context {
 	size_t remaining;
 };
 
+struct lwm2m_senml_json_context {
+	bool base_name_stored : 1;
+	bool full_name_true : 1;
+	uint8_t base64_buf_len : 2;
+	uint8_t base64_mod_buf[3];
+	uint8_t json_flags;
+	struct lwm2m_obj_path base_name_path;
+	uint8_t resource_path_level;
+};
+
 struct lwm2m_block_context {
 	struct coap_block_context ctx;
 	struct lwm2m_opaque_context opaque;
+#if defined(CONFIG_LWM2M_RW_SENML_JSON_SUPPORT)
+struct lwm2m_senml_json_context senml_json_ctx;
+#endif
 	int64_t timestamp;
 	uint32_t expected;
 	uint8_t token[8];