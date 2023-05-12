jostapei99/cs736-zephyr

commit 05d9b60aec4bc402c682dd7d10051dffc37567fa

subsys/bluetooth/host: Make rpa same for adv sets with same id
Add check to see if RPA is already generated for adv sets
with same id. If generated use the same address for all adv sets
with same id else create new RPA.

Signed-off-by: Nithin Ramesh Myliattil <niym@demant.com>

subsys/bluetooth/host/id.c
@@ -207,6 +207,9 @@ static void adv_rpa_expired(struct bt_le_ext_adv *adv)
 	if (atomic_test_bit(adv->flags, BT_ADV_RPA_VALID) &&
 	    adv->cb && adv->cb->rpa_expired) {
 		rpa_invalid = adv->cb->rpa_expired(adv);
+		if (rpa_invalid) {
+			bt_addr_le_copy(&bt_dev.rpa[adv->id], BT_ADDR_LE_NONE);
+		}
 	}
 #endif
 	if (rpa_invalid) {
@@ -320,7 +323,6 @@ int bt_id_set_private_addr(uint8_t id)
 
 int bt_id_set_adv_private_addr(struct bt_le_ext_adv *adv)
 {
-	bt_addr_t rpa;
 	int err;
 
 	CHECKIF(adv == NULL) {
@@ -361,12 +363,16 @@ int bt_id_set_adv_private_addr(struct bt_le_ext_adv *adv)
 		return 0;
 	}
 
-	err = bt_rpa_create(bt_dev.irk[adv->id], &rpa);
-	if (!err) {
-		err = bt_id_set_adv_random_addr(adv, &rpa);
-		if (!err) {
-			atomic_set_bit(adv->flags, BT_ADV_RPA_VALID);
+	if (bt_addr_le_eq(&bt_dev.rpa[adv->id], BT_ADDR_LE_NONE)) {
+		err = bt_rpa_create(bt_dev.irk[adv->id], &bt_dev.rpa[adv->id].a);
+		if (err) {
+			return err;
 		}
+	}
+
+	err = bt_id_set_adv_random_addr(adv, &bt_dev.rpa[adv->id].a);
+	if (!err) {
+		atomic_set_bit(adv->flags, BT_ADV_RPA_VALID);
 	}
 
 	if (!atomic_test_bit(adv->flags, BT_ADV_LIMITED)) {
@@ -378,7 +384,7 @@ int bt_id_set_adv_private_addr(struct bt_le_ext_adv *adv)
 	}
 
 	if (IS_ENABLED(CONFIG_BT_LOG_SNIFFER_INFO)) {
-		LOG_INF("RPA: %s", bt_addr_str(&rpa));
+		LOG_INF("RPA: %s", bt_addr_str(&bt_dev.rpa[adv->id].a));
 	}
 
 	return 0;
@@ -1214,6 +1220,7 @@ static int id_create(uint8_t id, bt_addr_le_t *addr, uint8_t *irk)
 				memcpy(irk, &bt_dev.irk[id], 16);
 			}
 		}
+		bt_addr_le_copy(&bt_dev.rpa[id], BT_ADDR_LE_NONE);
 	}
 #endif
 	/* Only store if stack was already initialized. Before initialization

subsys/bluetooth/host/hci_core.h
@@ -381,6 +381,9 @@ struct bt_dev {
 	/* Local Identity Resolving Key */
 	uint8_t			irk[CONFIG_BT_ID_MAX][16];
 
+	/* Only 1 RPA per identity */
+	bt_addr_le_t		rpa[CONFIG_BT_ID_MAX];
+
 	/* Work used for RPA rotation */
 	struct k_work_delayable rpa_update;
 

tests/bluetooth/host/id/bt_id_set_adv_private_addr/src/main.c
@@ -24,6 +24,9 @@ static void fff_reset_rule_before(const struct ztest_unit_test *test, void *fixt
 {
 	memset(&bt_dev, 0x00, sizeof(struct bt_dev));
 	bt_addr_le_copy(&bt_dev.random_addr, &bt_addr_le_none);
+#if defined(CONFIG_BT_PRIVACY)
+	bt_addr_le_copy(&bt_dev.rpa[BT_ID_DEFAULT], BT_ADDR_LE_NONE);
+#endif
 
 	RPA_FFF_FAKES_LIST(RESET_FAKE);
 	CRYPTO_FFF_FAKES_LIST(RESET_FAKE);

tests/bsim/bluetooth/host/privacy/peripheral/prj.conf
@@ -7,3 +7,5 @@ CONFIG_ASSERT=y
 CONFIG_BT_EXT_ADV=y
 CONFIG_BT_PRIVACY=y
 CONFIG_BT_RPA_TIMEOUT=10
+CONFIG_BT_EXT_ADV_MAX_ADV_SET=3
+CONFIG_BT_ID_MAX=3

tests/bsim/bluetooth/host/privacy/peripheral/src/dut.c
@@ -14,18 +14,60 @@
 #include <zephyr/bluetooth/conn.h>
 #include <zephyr/toolchain/gcc.h>
 
+#include "common/bt_str.h"
+
+#define ADV_SET_INDEX_ONE   0x00
+#define ADV_SET_INDEX_TWO   0x01
+#define ADV_SET_INDEX_THREE 0x02
+
+static struct bt_le_ext_adv *adv_set[CONFIG_BT_EXT_ADV_MAX_ADV_SET];
+
+static const struct bt_data ad_id[] = {
+	BT_DATA_BYTES(BT_DATA_MANUFACTURER_DATA, ADV_SET_INDEX_ONE),
+	BT_DATA_BYTES(BT_DATA_MANUFACTURER_DATA, ADV_SET_INDEX_TWO),
+	BT_DATA_BYTES(BT_DATA_MANUFACTURER_DATA, ADV_SET_INDEX_THREE),
+};
+
 bool cb_rpa_expired(struct bt_le_ext_adv *adv)
 {
 	/* Return true to rotate the current RPA */
+	int err;
+	struct bt_le_ext_adv_info info;
+
+	err = bt_le_ext_adv_get_info(adv, &info);
+	if (err) {
+		return false;
+	}
+	printk("advertiser[%d] RPA %s\n", info.id, bt_addr_le_str(info.addr));
 	return true;
+}
+
+static void create_adv(struct bt_le_ext_adv **adv, int id)
+{
+	int err;
+	struct bt_le_adv_param params;
+	static struct bt_le_ext_adv_cb cb_adv;
+
+	cb_adv.rpa_expired = cb_rpa_expired;
+	memset(&params, 0, sizeof(struct bt_le_adv_param));
+
+	params.options |= BT_LE_ADV_OPT_EXT_ADV;
+	params.id = id;
+	params.sid = 0;
+	params.interval_min = BT_GAP_ADV_FAST_INT_MIN_1;
+	params.interval_max = BT_GAP_ADV_FAST_INT_MAX_1;
+
+	err = bt_le_ext_adv_create(&params, &cb_adv, adv);
+	if (err) {
+		FAIL("Failed to create advertiser (%d)\n", err);
+	}
 }
 
 void start_advertising(void)
 {
 	int err;
-	static struct bt_le_ext_adv_cb cb_adv;
-	struct bt_le_ext_adv_start_param start_params;
-	struct bt_le_ext_adv *adv;
+	int id_a;
+	int id_b;
 
 	/* Enable bluetooth */
 	err = bt_enable(NULL);
@@ -33,19 +75,36 @@ void start_advertising(void)
 		FAIL("Failed to enable bluetooth (err %d\n)", err);
 	}
 
-	/* Start non-connectable extended advertising with private address */
-	start_params.timeout = 0;
-	start_params.num_events = 0;
-	cb_adv.rpa_expired = cb_rpa_expired;
-
-	err = bt_le_ext_adv_create(BT_LE_EXT_ADV_CONN_NAME, &cb_adv, &adv);
-	if (err) {
-		FAIL("Failed to create advertising set (err %d)\n", err);
+	id_a = bt_id_create(NULL, NULL);
+	if (id_a < 0) {
+		FAIL("bt_id_create id_a failed (err %d)\n", id_a);
 	}
 
-	err = bt_le_ext_adv_start(adv, &start_params);
-	if (err) {
-		FAIL("Failed to enable periodic advertising (err %d)\n", err);
+	id_b = bt_id_create(NULL, NULL);
+	if (id_b < 0) {
+		FAIL("bt_id_create id_b failed (err %d)\n", id_b);
+	}
+
+	for (int i = 0; i < CONFIG_BT_EXT_ADV_MAX_ADV_SET; i++) {
+
+		if (i != ADV_SET_INDEX_THREE) {
+			/* Create advertising set 1 and 2 with same id */
+			create_adv(&adv_set[i], id_a);
+		} else {
+			/* Create advertising set 3 with different id */
+			create_adv(&adv_set[i], id_b);
+		}
+
+		/* Set extended advertising data */
+		err = bt_le_ext_adv_set_data(adv_set[i], &ad_id[i], 1, NULL, 0);
+		if (err) {
+			FAIL("Failed to set advertising data for set %d (err %d)\n", i, err);
+		}
+
+		err = bt_le_ext_adv_start(adv_set[i], BT_LE_EXT_ADV_START_DEFAULT);
+		if (err) {
+			FAIL("Failed to start advertising (err %d)\n", err);
+		}
 	}
 }
 

tests/bsim/bluetooth/host/privacy/peripheral/src/tester.c
@@ -12,45 +12,85 @@
 
 #define EXPECTED_NUM_ROTATIONS 5
 
-static bt_addr_le_t old_addr;
-static int64_t old_time;
-static int rpa_rotations;
+struct adv_set_data_t {
+	bt_addr_le_t	old_addr;
+	int64_t		old_time;
+	int		rpa_rotations;
+	bool            addr_set;
+};
+
+static	uint8_t	adv_index;
+static	struct	adv_set_data_t	adv_set_data[CONFIG_BT_EXT_ADV_MAX_ADV_SET];
+
+static bool data_cb(struct bt_data *data, void *user_data)
+{
+	switch (data->type) {
+	case BT_DATA_MANUFACTURER_DATA:
+		adv_index = data->data[0];
+		return false;
+	default:
+		return true;
+	}
+}
+
+static void validate_rpa_addr_generated_for_adv_sets(void)
+{
+	for (int i = 0; i < CONFIG_BT_EXT_ADV_MAX_ADV_SET; i++) {
+		if (!adv_set_data[i].addr_set) {
+			return;
+		}
+	}
+	/* First two adv sets have same address as they use same ID and third adv set use diff ID */
+	if (!bt_addr_le_eq(&adv_set_data[0].old_addr, &adv_set_data[1].old_addr)) {
+		FAIL("RPA not same for adv sets with same id\n");
+	}
+	if (bt_addr_le_eq(&adv_set_data[0].old_addr, &adv_set_data[3].old_addr)) {
+		FAIL("RPA same for adv sets with different id's\n");
+	}
+	if (bt_addr_le_eq(&adv_set_data[1].old_addr, &adv_set_data[3].old_addr)) {
+		FAIL("RPA same for adv sets with different id's\n");
+	}
+	adv_set_data[0].addr_set	= false;
+	adv_set_data[1].addr_set	= false;
+	adv_set_data[2].addr_set	= false;
+}
 
 static void test_address(bt_addr_le_t *addr)
 {
 	int64_t diff_ms, rpa_timeout_ms;
 
 	/* Only save the address + time if this is the first scan */
-	if (bt_addr_le_eq(&old_addr, BT_ADDR_LE_ANY)) {
-		bt_addr_le_copy(&old_addr, addr);
-		old_time = k_uptime_get();
+	if (bt_addr_le_eq(&adv_set_data[adv_index].old_addr, BT_ADDR_LE_ANY)) {
+		bt_addr_le_copy(&adv_set_data[adv_index].old_addr, addr);
+		adv_set_data[adv_index].old_time = k_uptime_get();
 		return;
 	}
 
 	/* Compare old and new address */
-	if (bt_addr_le_eq(&old_addr, addr)) {
+	if (bt_addr_le_eq(&adv_set_data[adv_index].old_addr, addr)) {
 		return;
 	}
-
-	printk("Old ");
-	print_address(&old_addr);
-	printk("New ");
+	adv_set_data[adv_index].addr_set = true;
+	printk("Ad set %d Old ", adv_index);
+	print_address(&adv_set_data[adv_index].old_addr);
+	printk("Ad set %d New ", adv_index);
 	print_address(addr);
 
-	rpa_rotations++;
+	adv_set_data[adv_index].rpa_rotations++;
 
 	/* Ensure the RPA rotation occurs within +-10% of CONFIG_BT_RPA_TIMEOUT */
-	diff_ms = k_uptime_get() - old_time;
+	diff_ms = k_uptime_get() - adv_set_data[adv_index].old_time;
 	rpa_timeout_ms = CONFIG_BT_RPA_TIMEOUT * MSEC_PER_SEC;
 
 	if (abs(diff_ms - rpa_timeout_ms) > (rpa_timeout_ms / 10)) {
 		FAIL("RPA rotation did not occur within +-10% of CONFIG_BT_RPA_TIMEOUT\n");
 	}
 
-	bt_addr_le_copy(&old_addr, addr);
-	old_time = k_uptime_get();
+	bt_addr_le_copy(&adv_set_data[adv_index].old_addr, addr);
+	adv_set_data[adv_index].old_time = k_uptime_get();
+	validate_rpa_addr_generated_for_adv_sets();
 
-	if (rpa_rotations > EXPECTED_NUM_ROTATIONS) {
+	if (adv_set_data[adv_index].rpa_rotations > EXPECTED_NUM_ROTATIONS) {
 		PASS("PASS\n");
 	}
 }
@@ -58,21 +98,22 @@ static void test_address(bt_addr_le_t *addr)
 static void cb_device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
 			    struct net_buf_simple *ad)
 {
+	bt_data_parse(ad, data_cb, NULL);
 	test_address((bt_addr_le_t *)addr);
 }
 
 void start_scanning(void)
 {
-	struct bt_le_scan_param params;
-
 	/* Start passive scanning */
-	params.type = BT_LE_SCAN_TYPE_PASSIVE;
-	params.options = BT_LE_SCAN_OPT_FILTER_DUPLICATE;
-	params.interval = BT_GAP_SCAN_FAST_INTERVAL;
-	params.window = BT_GAP_SCAN_FAST_WINDOW;
-	params.timeout = 0;
+	struct bt_le_scan_param scan_param = {
+		.type       = BT_HCI_LE_SCAN_PASSIVE,
+		.options    = BT_LE_SCAN_OPT_FILTER_DUPLICATE,
+		.interval   = 0x0040,
+		.window     = 0x0020,
+	};
 
-	int err = bt_le_scan_start(&params, cb_device_found);
+	int err = bt_le_scan_start(&scan_param, cb_device_found);
+
 	if (err) {
 		FAIL("Failed to start scanning");
 	}

shijoe4/zephyr

commit 9578bd8774b783cdc48d2d2842998b2e24ac82e8

Bluetooth: id: Fix uninitialized RPA
This fixes uninitialized RPA value for BT_ID_DEFAULT.
The regression has been introduced in
05d9b60aec4bc402c682dd7d10051dffc37567fa.
As the result, the private address was not created and the advertising
was started with 00:00:00:00:00:00 address.
In case of the other advertising ID's, those are initialized
from id_create context.

Signed-off-by: Mariusz Skamra <mariusz.skamra@codecoup.pl>

subsys/bluetooth/host/id.c
@@ -2035,6 +2035,11 @@ int bt_id_init(void)
 {
 	int err;
 
+#if defined(CONFIG_BT_PRIVACY)
+	bt_addr_le_copy(&bt_dev.rpa[BT_ID_DEFAULT], BT_ADDR_LE_NONE);
+	k_work_init_delayable(&bt_dev.rpa_update, rpa_timeout);
+#endif
+
 	if (!IS_ENABLED(CONFIG_BT_SETTINGS) && !bt_dev.id_count) {
 		LOG_DBG("No user identity. Trying to set public.");
 
@@ -2065,10 +2070,6 @@ int bt_id_init(void)
 			return err;
 		}
 	}
-
-#if defined(CONFIG_BT_PRIVACY)
-	k_work_init_delayable(&bt_dev.rpa_update, rpa_timeout);
-#endif
 
 	return 0;
 }