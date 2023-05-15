jostapei99/cs736-zephyr

commit 3b5d7ff4a41929a3ee3412efed5d96a9fc3a467a

drivers: all: mcux: remove conditional support for pinctrl
The MCUX platform always uses pinctrl, there's no need to keep extra
macrology around pinctrl. Also updated driver's Kconfig options to
`select PINCTRL` (note that some already did).

Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

drivers/i3c/i3c_mcux.c
@@ -18,9 +18,7 @@
 #include <zephyr/drivers/clock_control.h>
 #include <zephyr/drivers/i3c.h>
 
-#ifdef CONFIG_PINCTRL
 #include <zephyr/drivers/pinctrl.h>
-#endif
 
 /*
  * This is from NXP HAL which contains register bits macros
@@ -78,10 +76,8 @@ struct mcux_i3c_config {
 	/** Clock control subsys related struct. */
 	clock_control_subsys_t clock_subsys;
 
-#ifdef CONFIG_PINCTRL
 	/** Pointer to pin control device. */
 	const struct pinctrl_dev_config *pincfg;
-#endif
 
 	/** Interrupt configuration function. */
 	void (*irq_config_func)(const struct device *dev);
@@ -2006,12 +2002,10 @@ static int mcux_i3c_init(const struct device *dev)
 	CLOCK_SetClkDiv(kCLOCK_DivI3cSlowClk, data->clocks.clk_div_od);
 	CLOCK_SetClkDiv(kCLOCK_DivI3cTcClk, data->clocks.clk_div_tc);
 
-#ifdef CONFIG_PINCTRL
 	ret = pinctrl_apply_state(config->pincfg, PINCTRL_STATE_DEFAULT);
 	if (ret != 0) {
 		goto err_out;
 	}
-#endif
 
 	k_sem_init(&data->lock, 1, 1);
 	k_sem_init(&data->ibi_lock, 1, 1);
@@ -2120,16 +2114,8 @@ static const struct i3c_driver_api mcux_i3c_driver_api = {
 #endif
 };
 
-#ifdef CONFIG_PINCTRL
-#define I3C_MCUX_PINCTRL_DEFINE(n) PINCTRL_DT_INST_DEFINE(n);
-#define I3C_MCUX_PINCTRL_INIT(n) .pincfg = PINCTRL_DT_INST_DEV_CONFIG_GET(n),
-#else
-#define I3C_MCUX_PINCTRL_DEFINE(n)
-#define I3C_MCUX_PINCTRL_INIT(n)
-#endif
-
 #define I3C_MCUX_DEVICE(id)							\
-	I3C_MCUX_PINCTRL_DEFINE(id)						\
+	PINCTRL_DT_INST_DEFINE(n);						\
 	static void mcux_i3c_config_func_##id(const struct device *dev);	\
 	static struct i3c_device_desc mcux_i3c_device_array_##id[] =			\
 		I3C_DEVICE_ARRAY_DT_INST(id);					\
@@ -2145,7 +2131,7 @@ static const struct i3c_driver_api mcux_i3c_driver_api = {
 		.common.dev_list.num_i3c = ARRAY_SIZE(mcux_i3c_device_array_##id),	\
 		.common.dev_list.i2c = mcux_i3c_i2c_device_array_##id,			\
 		.common.dev_list.num_i2c = ARRAY_SIZE(mcux_i3c_i2c_device_array_##id),	\
-		I3C_MCUX_PINCTRL_INIT(id)					\
+		.pincfg = PINCTRL_DT_INST_DEV_CONFIG_GET(n),			\
 	};									\
 	static struct mcux_i3c_data mcux_i3c_data_##id = {			\
 		.clocks.i3c_od_scl_hz = DT_INST_PROP_OR(id, i3c_od_scl_hz, 0),	\

drivers/adc/adc_mcux_lpadc.c
@@ -14,9 +14,7 @@
 #include <zephyr/drivers/adc.h>
 #include <fsl_lpadc.h>
 
-#if CONFIG_PINCTRL
 #include <zephyr/drivers/pinctrl.h>
-#endif
 
 #if !defined(CONFIG_SOC_SERIES_IMX_RT11XX)
 #include <fsl_power.h>
@@ -58,9 +56,7 @@ struct mcux_lpadc_config {
 	uint32_t offset_a;
 	uint32_t offset_b;
 	void (*irq_config_func)(const struct device *dev);
-#if CONFIG_PINCTRL
 	const struct pinctrl_dev_config *pincfg;
-#endif
 };
 
 struct mcux_lpadc_data {
@@ -403,14 +399,12 @@ static int mcux_lpadc_init(const struct device *dev)
 	struct mcux_lpadc_data *data = dev->data;
 	ADC_Type *base = config->base;
 	lpadc_config_t adc_config;
-#ifdef CONFIG_PINCTRL
 	int err;
 
 	err = pinctrl_apply_state(config->pincfg, PINCTRL_STATE_DEFAULT);
 	if (err) {
 		return err;
 	}
-#endif
 
 #if !defined(CONFIG_SOC_SERIES_IMX_RT11XX)
 #if	defined(CONFIG_SOC_SERIES_IMX_RT6XX)
@@ -556,14 +550,6 @@ static const struct adc_driver_api mcux_lpadc_driver_api = {
 #define TO_LPADC_POWER_LEVEL(val) \
 	_DO_CONCAT(kLPADC_PowerLevelAlt, val)
 
-#if CONFIG_PINCTRL
-#define PINCTRL_DEFINE(n) PINCTRL_DT_INST_DEFINE(n);
-#define PINCTRL_INIT(n) .pincfg = PINCTRL_DT_INST_DEV_CONFIG_GET(n),
-#else
-#define PINCTRL_DEFINE(n)
-#define PINCTRL_INIT(n)
-#endif /* CONFIG_PINCTRL */
-
 #define LPADC_MCUX_INIT(n)						\
 	static void mcux_lpadc_config_func_##n(const struct device *dev);	\
 									\
@@ -578,7 +564,7 @@ static const struct adc_driver_api mcux_lpadc_driver_api = {
 		"Invalid conversion average number for auto-calibration time");	\
 	ASSERT_WITHIN_RANGE(DT_INST_PROP(n, power_level), 1, 4,		\
 		"Invalid power level");					\
-	PINCTRL_DEFINE(n)						\
+	PINCTRL_DT_INST_DEFINE(n);					\
 	static const struct mcux_lpadc_config mcux_lpadc_config_##n = {	\
 		.base = (ADC_Type *)DT_INST_REG_ADDR(n),	\
 		.clock_source = TO_LPADC_CLOCK_SOURCE(DT_INST_PROP(n, clk_source)),	\
@@ -591,7 +577,7 @@ static const struct adc_driver_api mcux_lpadc_driver_api = {
 		.offset_a = DT_INST_PROP(n, offset_value_a),	\
 		.offset_b = DT_INST_PROP(n, offset_value_b),	\
 		.irq_config_func = mcux_lpadc_config_func_##n,				\
-		PINCTRL_INIT(n)					\
+		.pincfg = PINCTRL_DT_INST_DEV_CONFIG_GET(n),			\
 	};									\
 										\
 	static struct mcux_lpadc_data mcux_lpadc_data_##n = {	\

drivers/can/Kconfig.mcux
@@ -8,6 +8,7 @@ config CAN_MCUX_FLEXCAN
 	default y
 	depends on DT_HAS_NXP_FLEXCAN_ENABLED
 	depends on CLOCK_CONTROL
+	select PINCTRL
 	help
 	  Enable support for the NXP FlexCAN driver.
 
@@ -37,5 +38,6 @@ config CAN_MCUX_MCAN
 	depends on DT_HAS_NXP_LPC_MCAN_ENABLED
 	depends on CLOCK_CONTROL
 	select CAN_MCAN
+	select PINCTRL
 	help
 	  Enable support for mcux mcan driver.

drivers/can/can_mcux_flexcan.c
@@ -20,10 +20,7 @@
 #include <fsl_flexcan.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/irq.h>
-
-#ifdef CONFIG_PINCTRL
 #include <zephyr/drivers/pinctrl.h>
-#endif
 
 LOG_MODULE_REGISTER(can_mcux_flexcan, CONFIG_CAN_LOG_LEVEL);
 
@@ -107,9 +104,7 @@ struct mcux_flexcan_config {
 	void (*irq_disable_func)(void);
 	const struct device *phy;
 	uint32_t max_bitrate;
-#ifdef CONFIG_PINCTRL
 	const struct pinctrl_dev_config *pincfg;
-#endif /* CONFIG_PINCTRL */
 };
 
 struct mcux_flexcan_rx_callback {
@@ -1238,12 +1233,10 @@ static int mcux_flexcan_init(const struct device *dev)
 	}
 #endif /* CONFIG_CAN_MCUX_FLEXCAN_FD */
 
-#ifdef CONFIG_PINCTRL
 	err = pinctrl_apply_state(config->pincfg, PINCTRL_STATE_DEFAULT);
 	if (err != 0) {
 		return err;
 	}
-#endif
 
 	err = mcux_flexcan_get_core_clock(dev, &clock_freq);
 	if (err != 0) {
@@ -1434,14 +1427,6 @@ static const struct can_driver_api mcux_flexcan_fd_driver_api = {
 		FLEXCAN_IRQ_ENABLE_CODE(node_id, prop, idx); \
 	} while (false);
 
-#ifdef CONFIG_PINCTRL
-#define FLEXCAN_PINCTRL_DEFINE(id) PINCTRL_DT_INST_DEFINE(id);
-#define FLEXCAN_PINCTRL_INIT(id) .pincfg = PINCTRL_DT_INST_DEV_CONFIG_GET(id),
-#else
-#define FLEXCAN_PINCTRL_DEFINE(id)
-#define FLEXCAN_PINCTRL_INIT(id)
-#endif /* CONFIG_PINCTRL */
-
 #ifdef CONFIG_CAN_MCUX_FLEXCAN_FD
 #define FLEXCAN_MAX_BITRATE(id)							\
 	COND_CODE_1(DT_NODE_HAS_COMPAT(DT_DRV_INST(id), FLEXCAN_FD_DRV_COMPAT),	\
@@ -1460,7 +1445,7 @@ static const struct can_driver_api mcux_flexcan_fd_driver_api = {
 #endif /* !CONFIG_CAN_MCUX_FLEXCAN_FD */
 
 #define FLEXCAN_DEVICE_INIT_MCUX(id)					\
-	FLEXCAN_PINCTRL_DEFINE(id)					\
+	PINCTRL_DT_INST_DEFINE(id);					\
 									\
 	static void mcux_flexcan_irq_config_##id(const struct device *dev); \
 	static void mcux_flexcan_irq_enable_##id(void); \
@@ -1493,7 +1478,7 @@ static const struct can_driver_api mcux_flexcan_fd_driver_api = {
 		.phy = DEVICE_DT_GET_OR_NULL(DT_INST_PHANDLE(id, phys)),\
 		.max_bitrate = DT_INST_CAN_TRANSCEIVER_MAX_BITRATE(id,	\
 			FLEXCAN_MAX_BITRATE(id)),			\
-		FLEXCAN_PINCTRL_INIT(id)				\
+		.pincfg = PINCTRL_DT_INST_DEV_CONFIG_GET(id),		\
 	};								\
 									\
 	static struct mcux_flexcan_data mcux_flexcan_data_##id;		\

drivers/can/can_mcux_mcan.c
@@ -7,9 +7,7 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/can.h>
 #include <zephyr/drivers/clock_control.h>
-#ifdef CONFIG_PINCTRL
 #include <zephyr/drivers/pinctrl.h>
-#endif
 #include <zephyr/logging/log.h>
 #include <zephyr/irq.h>
 
@@ -23,9 +21,7 @@ struct mcux_mcan_config {
 	const struct device *clock_dev;
 	clock_control_subsys_t clock_subsys;
 	void (*irq_config_func)(const struct device *dev);
-#ifdef CONFIG_PINCTRL
 	const struct pinctrl_dev_config *pincfg;
-#endif
 };
 
 struct mcux_mcan_data {
@@ -52,12 +48,10 @@ static int mcux_mcan_init(const struct device *dev)
 		return -ENODEV;
 	}
 
-#ifdef CONFIG_PINCTRL
 	err = pinctrl_apply_state(mcux_config->pincfg, PINCTRL_STATE_DEFAULT);
 	if (err) {
 		return err;
 	}
-#endif /* CONFIG_PINCTRL */
 
 	err = clock_control_on(mcux_config->clock_dev, mcux_config->clock_subsys);
 	if (err) {
@@ -142,16 +136,8 @@ static const struct can_driver_api mcux_mcan_driver_api = {
 #endif /* CONFIG_CAN_FD_MODE */
 };
 
-#ifdef CONFIG_PINCTRL
-#define MCUX_MCAN_PINCTRL_DEFINE(n) PINCTRL_DT_INST_DEFINE(n)
-#define MCUX_MCAN_PINCTRL_INIT(n) .pincfg = PINCTRL_DT_INST_DEV_CONFIG_GET(n),
-#else
-#define MCUX_MCAN_PINCTRL_DEFINE(n)
-#define MCUX_MCAN_PINCTRL_INIT(n)
-#endif
-
 #define MCUX_MCAN_INIT(n)						\
-	MCUX_MCAN_PINCTRL_DEFINE(n);					\
+	PINCTRL_DT_INST_DEFINE(n);					\
 									\
 	static void mcux_mcan_irq_config_##n(const struct device *dev); \
 									\
@@ -160,7 +146,7 @@ static const struct can_driver_api mcux_mcan_driver_api = {
 		.clock_subsys = (clock_control_subsys_t)		\
 			DT_INST_CLOCKS_CELL(n, name),			\
 		.irq_config_func = mcux_mcan_irq_config_##n,		\
-		MCUX_MCAN_PINCTRL_INIT(n)				\
+		.pincfg = PINCTRL_DT_INST_DEV_CONFIG_GET(n),		\
 	};								\
 									\
 	static const struct can_mcan_config can_mcan_config_##n =	\

drivers/gpio/Kconfig.mcux_igpio
@@ -8,5 +8,6 @@ config GPIO_MCUX_IGPIO
 	default y
 	depends on HAS_MCUX_IGPIO
 	depends on DT_HAS_NXP_IMX_GPIO_ENABLED
+	select PINCTRL
 	help
 	  Enable the MCUX IGPIO driver.

drivers/gpio/gpio_mcux_igpio.c
@@ -14,9 +14,7 @@
 #include <fsl_common.h>
 #include <fsl_gpio.h>
 
-#ifdef CONFIG_PINCTRL
 #include <zephyr/drivers/pinctrl.h>
-#endif
 
 #include <zephyr/drivers/gpio/gpio_utils.h>
 
@@ -29,12 +27,10 @@ struct mcux_igpio_config {
 	/* gpio_driver_config needs to be first */
 	struct gpio_driver_config common;
 	GPIO_Type *base;
-#ifdef CONFIG_PINCTRL
 	const struct pinctrl_soc_pinmux *pin_muxes;
 	const struct gpio_pin_gaps *pin_gaps;
 	uint8_t mux_count;
 	uint8_t gap_count;
-#endif
 };
 
 struct mcux_igpio_data {
@@ -50,7 +46,6 @@ static int mcux_igpio_configure(const struct device *dev,
 	const struct mcux_igpio_config *config = dev->config;
 	GPIO_Type *base = config->base;
 
-#ifdef CONFIG_PINCTRL
 	struct pinctrl_soc_pin pin_cfg;
 	int cfg_idx = pin, i;
 
@@ -195,16 +190,6 @@ static int mcux_igpio_configure(const struct device *dev,
 	/* cfg register will be set by pinctrl_configure_pins */
 	pin_cfg.pin_ctrl_flags = reg;
 	pinctrl_configure_pins(&pin_cfg, 1, PINCTRL_REG_NONE);
-#else
-	/* Without pinctrl, no support for GPIO flags */
-	if ((flags & GPIO_SINGLE_ENDED) != 0) {
-		return -ENOTSUP;
-	}
-
-	if (((flags & GPIO_PULL_UP) != 0) || ((flags & GPIO_PULL_DOWN) != 0)) {
-		return -ENOTSUP;
-	}
-#endif /* CONFIG_PINCTRL */
 
 	if (((flags & GPIO_INPUT) != 0) && ((flags & GPIO_OUTPUT) != 0)) {
 		return -ENOTSUP;
@@ -366,7 +351,6 @@ static const struct gpio_driver_api mcux_igpio_driver_api = {
 };
 
 
-#ifdef CONFIG_PINCTRL
 /* These macros will declare an array of pinctrl_soc_pinmux types */
 #define PINMUX_INIT(node, prop, idx) MCUX_IMX_PINMUX(DT_PROP_BY_IDX(node, prop, idx)),
 #define MCUX_IGPIO_PIN_DECLARE(n)						\
@@ -380,10 +364,6 @@ static const struct gpio_driver_api mcux_igpio_driver_api = {
 	.pin_gaps = (const struct gpio_pin_gaps *)mcux_igpio_pin_gaps_##n,	\
 	.mux_count = DT_PROP_LEN(DT_DRV_INST(n), pinmux),			\
 	.gap_count = (ARRAY_SIZE(mcux_igpio_pin_gaps_##n) / 2)
-#else
-#define MCUX_IGPIO_PIN_DECLARE(n)
-#define MCUX_IGPIO_PIN_INIT(n)
-#endif /* CONFIG_PINCTRL */
 
 #define MCUX_IGPIO_IRQ_INIT(n, i)					\
 	do {								\

drivers/i2c/Kconfig
@@ -121,6 +121,7 @@ config I2C_MCUX_FLEXCOMM
 	bool "MCUX FLEXCOMM I2C driver"
 	default y
 	depends on DT_HAS_NXP_LPC_I2C_ENABLED
+	select PINCTRL
 	help
 	  Enable the mcux flexcomm i2c driver.
 
@@ -129,6 +130,7 @@ config I2C_MCUX_LPI2C
 	default y
 	depends on DT_HAS_NXP_IMX_LPI2C_ENABLED
 	depends on CLOCK_CONTROL
+	select PINCTRL
 	help
 	  Enable the mcux LPI2C driver.
 

shijoe4/zephyr

commit 1d6b32fbe2d3cc78e89e256b0dff9e52e7903425

drivers: i3c: Fix Build failure on MCUX I3C
Fix build failure introduced by commits
3b5d7ff4a41929a3ee3412efed5d96a9fc3a467a and
64c18627eabc2289bf3a1d7607bb3bc12746c8fa

Signed-off-by: Mahesh Mahadevan <mahesh.mahadevan@nxp.com>

drivers/i3c/i3c_mcux.c
@@ -832,7 +832,7 @@ struct i3c_device_desc *mcux_i3c_device_find(const struct device *dev,
 static struct i3c_i2c_device_desc *
 mcux_i3c_i2c_device_find(const struct device *dev, uint16_t addr)
 {
-	struct cdns_i3c_data *data = dev->data;
+	struct mcux_i3c_data *data = dev->data;
 
 	return i3c_dev_list_i2c_addr_find(&data->common.attached_dev, addr);
 }
@@ -2115,7 +2115,7 @@ static const struct i3c_driver_api mcux_i3c_driver_api = {
 };
 
 #define I3C_MCUX_DEVICE(id)							\
-	PINCTRL_DT_INST_DEFINE(n);						\
+	PINCTRL_DT_INST_DEFINE(id);						\
 	static void mcux_i3c_config_func_##id(const struct device *dev);	\
 	static struct i3c_device_desc mcux_i3c_device_array_##id[] =			\
 		I3C_DEVICE_ARRAY_DT_INST(id);					\
@@ -2131,7 +2131,7 @@ static const struct i3c_driver_api mcux_i3c_driver_api = {
 		.common.dev_list.num_i3c = ARRAY_SIZE(mcux_i3c_device_array_##id),	\
 		.common.dev_list.i2c = mcux_i3c_i2c_device_array_##id,			\
 		.common.dev_list.num_i2c = ARRAY_SIZE(mcux_i3c_i2c_device_array_##id),	\
-		.pincfg = PINCTRL_DT_INST_DEV_CONFIG_GET(n),			\
+		.pincfg = PINCTRL_DT_INST_DEV_CONFIG_GET(id),			\
 	};									\
 	static struct mcux_i3c_data mcux_i3c_data_##id = {			\
 		.clocks.i3c_od_scl_hz = DT_INST_PROP_OR(id, i3c_od_scl_hz, 0),	\