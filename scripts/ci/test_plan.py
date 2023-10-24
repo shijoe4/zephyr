PerMac/sdk-zephyr

scripts: Add arg to set_plan.py for alternative tag relation list
Add --alt-tag arg for test_plan.py script. User can use it and point
to an alternative file with tag-directories relations. If so, such
file will be used instead of the default one.

Signed-off-by: Maciej Perkowski <Maciej.Perkowski@nordicsemi.no>

scripts/ci/test_plan.py
@@ -85,7 +85,7 @@ class Tag:
         return "<Tag {}>".format(self.name)
 
 class Filters:
-    def __init__(self, modified_files, pull_request=False, platforms=[], no_path_name = False, ignore_path=None):
+    def __init__(self, modified_files, pull_request=False, platforms=[], no_path_name = False, ignore_path=None, alt_tags=None):
         self.modified_files = modified_files
         self.twister_options = []
         self.full_twister = False
@@ -95,6 +95,9 @@ class Filters:
         self.platforms = platforms
         self.default_run = False
         self.no_path_name = no_path_name
+        self.tag_cfg_file = os.path.join(zephyr_base, 'scripts', 'ci', 'tags.yaml')
+        if alt_tags:
+            self.tag_cfg_file = alt_tags
         self.ignore_path = f"{zephyr_base}/scripts/ci/twister_ignore.txt"
         if ignore_path:
             self.ignore_path = ignore_path
@@ -231,8 +234,7 @@ class Filters:
 
     def find_tags(self):
 
-        tag_cfg_file = os.path.join(zephyr_base, 'scripts', 'ci', 'tags.yaml')
-        with open(tag_cfg_file, 'r') as ymlfile:
+        with open(self.tag_cfg_file, 'r') as ymlfile:
             tags_config = yaml.safe_load(ymlfile)
 
         tags = {}
@@ -325,6 +327,8 @@ def parse_args():
             help="Don't put paths into test suites' names ")
     parser.add_argument('--ignore-path', default=None,
             help="Path to a text file with patterns of files to be matched against changed files")
+    parser.add_argument('--alt-tags', default=None,
+            help="Path to a file describing relations between directories and tags")
 
     return parser.parse_args()
 
@@ -350,7 +354,7 @@ if __name__ == "__main__":
         print("\n".join(files))
         print("=========")
 
-    f = Filters(files, args.pull_request, args.platform, args.no_path_name, args.ignore_path)
+    f = Filters(files, args.pull_request, args.platform, args.no_path_name, args.ignore_path, args.alt_tags)
     f.process()
 
     # remove dupes and filtered cases

shijoe4/zephyr

scripts/ci/test_plan.py
@@ -93,7 +93,7 @@ class Tag:
         return "<Tag {}>".format(self.name)
 
 class Filters:
-    def __init__(self, modified_files, ignore_path, pull_request=False, platforms=[], detailed_test_id=True):
+    def __init__(self, modified_files, ignore_path, alt_tags, pull_request=False, platforms=[], detailed_test_id=True):
         self.modified_files = modified_files
         self.twister_options = []
         self.full_twister = False
@@ -104,6 +104,7 @@ class Filters:
         self.default_run = False
         self.detailed_test_id = detailed_test_id
         self.ignore_path = ignore_path
+        self.tag_cfg_file = alt_tags
 
     def process(self):
         self.find_modules()
@@ -280,8 +281,7 @@ class Filters:
 
     def find_tags(self):
 
-        tag_cfg_file = os.path.join(zephyr_base, 'scripts', 'ci', 'tags.yaml')
-        with open(tag_cfg_file, 'r') as ymlfile:
+        with open(self.tag_cfg_file, 'r') as ymlfile:
             tags_config = yaml.safe_load(ymlfile)
 
         tags = {}
@@ -377,6 +377,9 @@ def parse_args():
     parser.add_argument('--ignore-path',
             default=os.path.join(zephyr_base, 'scripts', 'ci', 'twister_ignore.txt'),
             help="Path to a text file with patterns of files to be matched against changed files")
+    parser.add_argument('--alt-tags',
+            default=os.path.join(zephyr_base, 'scripts', 'ci', 'tags.yaml'),
+            help="Path to a file describing relations between directories and tags")
 
     # Include paths in names by default.
     parser.set_defaults(detailed_test_id=True)
@@ -404,7 +407,7 @@ if __name__ == "__main__":
         print("\n".join(files))
         print("=========")
 
-    f = Filters(files, args.ignore_path, args.pull_request, args.platform, args.detailed_test_id)
+    f = Filters(files, args.ignore_path, args.alt_tags, args.pull_request, args.platform, args.detailed_test_id)
     f.process()
 
     # remove dupes and filtered cases