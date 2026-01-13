rado17/zephyr-rtos

drivers: wifi: nrf_wifi: Add RoC support
Add ops for remain-on-channel and cancelling remain-on-channel.

Signed-off-by: Ravi Dondaputi <ravi.dondaputi@nordicsemi.no>

drivers/wifi/nrf_wifi/inc/wpa_supp_if.h
@@ -123,8 +123,16 @@ int nrf_wifi_supp_get_conn_info(void *if_priv, struct wpa_conn_info *info);
 void nrf_wifi_supp_event_proc_get_conn_info(void *os_vif_ctx,
 					    struct nrf_wifi_umac_event_conn_info *info,
 					    unsigned int event_len);
+void nrf_wifi_supp_event_roc_complete(void *os_vif_ctx,
+					    struct nrf_wifi_event_remain_on_channel *info,
+					    unsigned int event_len);
+void nrf_wifi_supp_event_roc_cancel_complete(void *os_vif_ctx,
+					    struct nrf_wifi_event_remain_on_channel *info,
+					    unsigned int event_len);
 int nrf_wifi_supp_set_country(void *if_priv, const char *alpha2);
 int nrf_wifi_supp_get_country(void *if_priv, char *alpha2);
+int nrf_wifi_supp_remain_on_channel(void *if_priv, unsigned int freq, unsigned int duration);
+int nrf_wifi_supp_cancel_remain_on_channel(void *if_priv);
 
 #endif /* CONFIG_NRF70_STA_MODE */
 #ifdef CONFIG_NRF70_AP_MODE

drivers/wifi/nrf_wifi/src/fmac_main.c
@@ -843,6 +843,8 @@ static int nrf_wifi_drv_main_zep(const struct device *dev)
 	callbk_fns.event_get_wiphy = nrf_wifi_wpa_supp_event_get_wiphy;
 	callbk_fns.mgmt_rx_callbk_fn = nrf_wifi_wpa_supp_event_mgmt_rx_callbk_fn;
 	callbk_fns.get_conn_info_callbk_fn = nrf_wifi_supp_event_proc_get_conn_info;
+	callbk_fns.roc_callbk_fn = nrf_wifi_supp_event_roc_complete;
+	callbk_fns.roc_cancel_callbk_fn = nrf_wifi_supp_event_roc_cancel_complete;
 #endif /* CONFIG_NRF70_STA_MODE */
 
 	/* The OSAL layer needs to be initialized before any other initialization
@@ -963,6 +965,8 @@ static const struct zep_wpa_supp_dev_ops wpa_supp_ops = {
 	.get_conn_info = nrf_wifi_supp_get_conn_info,
 	.set_country = nrf_wifi_supp_set_country,
 	.get_country = nrf_wifi_supp_get_country,
+	.remain_on_channel = nrf_wifi_supp_remain_on_channel,
+	.cancel_remain_on_channel = nrf_wifi_supp_cancel_remain_on_channel,
 #ifdef CONFIG_NRF70_AP_MODE
 	.init_ap = nrf_wifi_wpa_supp_init_ap,
 	.start_ap = nrf_wifi_wpa_supp_start_ap,

shijoe4/zephyr

drivers/wifi/nrf_wifi/inc/wpa_supp_if.h
@@ -123,8 +123,16 @@ int nrf_wifi_supp_get_conn_info(void *if_priv, struct wpa_conn_info *info);
 void nrf_wifi_supp_event_proc_get_conn_info(void *os_vif_ctx,
 					    struct nrf_wifi_umac_event_conn_info *info,
 					    unsigned int event_len);
+void nrf_wifi_supp_event_remain_on_channel(void *os_vif_ctx,
+					    struct nrf_wifi_event_remain_on_channel *info,
+					    unsigned int event_len);
+void nrf_wifi_supp_event_roc_cancel_complete(void *os_vif_ctx,
+					    struct nrf_wifi_event_remain_on_channel *info,
+					    unsigned int event_len);
 int nrf_wifi_supp_set_country(void *if_priv, const char *alpha2);
 int nrf_wifi_supp_get_country(void *if_priv, char *alpha2);
+int nrf_wifi_supp_remain_on_channel(void *if_priv, unsigned int freq, unsigned int duration);
+int nrf_wifi_supp_cancel_remain_on_channel(void *if_priv);
 
 #endif /* CONFIG_NRF70_STA_MODE */
 #ifdef CONFIG_NRF70_AP_MODE

drivers/wifi/nrf_wifi/src/fmac_main.c
@@ -830,6 +830,8 @@ static int nrf_wifi_drv_main_zep(const struct device *dev)
 	callbk_fns.event_get_wiphy = nrf_wifi_wpa_supp_event_get_wiphy;
 	callbk_fns.mgmt_rx_callbk_fn = nrf_wifi_wpa_supp_event_mgmt_rx_callbk_fn;
 	callbk_fns.get_conn_info_callbk_fn = nrf_wifi_supp_event_proc_get_conn_info;
+	callbk_fns.roc_callbk_fn = nrf_wifi_supp_event_remain_on_channel;
+	callbk_fns.roc_cancel_callbk_fn = nrf_wifi_supp_event_roc_cancel_complete;
 #endif /* CONFIG_NRF70_STA_MODE */
 
 	/* The OSAL layer needs to be initialized before any other initialization
@@ -950,6 +952,8 @@ static const struct zep_wpa_supp_dev_ops wpa_supp_ops = {
 	.get_conn_info = nrf_wifi_supp_get_conn_info,
 	.set_country = nrf_wifi_supp_set_country,
 	.get_country = nrf_wifi_supp_get_country,
+	.remain_on_channel = nrf_wifi_supp_remain_on_channel,
+	.cancel_remain_on_channel = nrf_wifi_supp_cancel_remain_on_channel,
 #ifdef CONFIG_NRF70_AP_MODE
 	.init_ap = nrf_wifi_wpa_supp_init_ap,
 	.start_ap = nrf_wifi_wpa_supp_start_ap,