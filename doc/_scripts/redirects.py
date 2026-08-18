aesc-silicon/elements-zephyr

doc: releases: Rename elemrv to elemrv_flask
Fix the v4.2 release notes, mark the previous name as deprecated
and add a chapter to the v4.5 migration guide.

Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

doc/_scripts/redirects.py
@@ -17,6 +17,7 @@ REDIRECTS = (
     ('boards/arm/fvp_baser_aemv8r/doc/aarch32', 'boards/arm/fvp_baser_aemv8r/doc/index'),
     ('boards/arm/fvp_baser_aemv8r/doc/aarch64', 'boards/arm/fvp_baser_aemv8r/doc/index'),
     ('boards/arm/fvp_baser_aemv8r/doc/debug-with-arm-ds', 'boards/arm/fvp_baser_aemv8r/doc/index'),
+    ('boards/aesc/elemrv/doc/index', 'boards/aesc/elemrv_flask/doc/index'),
     ('boards/nxp/hexiwear/doc/index', 'boards/mikroe/hexiwear/doc/index'),
     ('boards/phytec/mimx8mm_phyboard_polis/doc/index', 'boards/phytec/phyboard_polis/doc/index'),
     ('boards/phytec/mimx8mp_phyboard_pollux/doc/index', 'boards/phytec/phyboard_pollux/doc/index'),

shijoe4/zephyr

doc/_scripts/redirects.py
@@ -14,6 +14,7 @@ Notes:
 
 REDIRECTS = (
     # zephyr-keep-sorted-start
+    ('boards/aesc/elemrv/doc/index', 'boards/aesc/elemrv_flask_n/doc/index'),
     ('boards/arm/fvp_baser_aemv8r/doc/aarch32', 'boards/arm/fvp_baser_aemv8r/doc/index'),
     ('boards/arm/fvp_baser_aemv8r/doc/aarch64', 'boards/arm/fvp_baser_aemv8r/doc/index'),
     ('boards/arm/fvp_baser_aemv8r/doc/debug-with-arm-ds', 'boards/arm/fvp_baser_aemv8r/doc/index'),